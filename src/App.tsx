2cf98fd28a1f4ee49d21849ce8f44f40511372ea
luiesker <monicalui@gmail.com>
2025-08-06 11:11:29 -0700

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Rollbar from 'rollbar';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p onClick={() => { throw new Error('This is a test error'); }}>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Rollbar from 'rollbar';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider, ErrorBoundary, LEVEL_WARN, RollbarContext, useRollbar } from '@rollbar/react' // <-- Provider imports 'rollbar' for us
 
-const rollbar = new Rollbar({
-  accessToken: 'YOUR_ACCESS_TOKEN',
+const rollbarConfig = {
+  accessToken: 'YOUR_TOKEN_HERE',
   captureUncaught: true,
   captureUnhandledRejections: true,
-});
+  environment: 'production',
+  recorder: {
+    enabled: true
+  },
+
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider config={rollbarConfig} >
+    <ErrorBoundary level={LEVEL_WARN}>
+
+      <App />
+    </ErrorBoundary>
+</Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
