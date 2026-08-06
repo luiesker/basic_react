d059c88cf555f4696412f7d552929909a3781617
luiesker <monicalui@gmail.com>
2026-08-06 10:36:35 -0700

    test

--- src/index.tsx
+++ src/index.tsx
@@ -20,13 +20,15 @@ const rollbarConfig = {
     enabled: true,
     blockClass: 'mask',
     maskAllInputs: true,
-    transformSpan: function(span: any) {
-      if (span.name !== 'rollbar-telemetry') return; // don't touch the DOM/rrweb span
-      span.events = span.events.filter((e: any) => {
-        if (e.name !== 'rollbar-network-event') return true;
-        if (e.data && e.data.body && e.data.body.subtype === 'xhr') { return false; }
-        return true;
-      });
+  },
+  tracing: {
+      transformSpan: function(span: any) {
+    if (span.name !== 'rollbar-telemetry') return; // don't touch the DOM/rrweb span
+    span.events = span.events.filter((e: any) => {
+      if (e.name !== 'rollbar-network-event') return true;
+      if (e.data && e.data.body && e.data.body.subtype === 'xhr') { return false; }
+      return true;
+    });
   },
   filterTelemetry: function(e: any) {
     return e.type === 'network'
